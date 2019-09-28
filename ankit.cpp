05138eebf5de8fd1933127cad9d937c4f5a3ea03
gauravDubeyDeveloper <52420891+gauravDubeyDeveloper@users.noreply.github.com>
2019-09-28 12:24:25 +0530

    i made cin in ankit

--- ankit.cpp
+++ ankit.cpp
@@ -1,10 +1,12 @@
 #include<iostream.h>
 using namespace std;
 int main(){
+int a;
 cout<<"Hello Ankit"<<endl;
 cout<<"hello shubham";
 cout<<"bye bye";
 cout<<"i am just testing";
+cin>>a;
 return 0;
 }
 
